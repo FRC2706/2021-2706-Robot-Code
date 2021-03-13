3c684df36498675655f81efd00c27e830b55f030
davidliu-2706 <dliu5@ocdsb.ca>
2021-03-13 13:56:13 -0500

    Small space changes

--- src/main/java/frc/robot/commands/VisionAssistedShooter.java
+++ src/main/java/frc/robot/commands/VisionAssistedShooter.java
@@ -31,7 +31,7 @@ public class VisionAssistedShooter extends CommandBase {
   private VisionCtrlNetTable visionControlNetTable;
 
   //calculated for the shooter
-  double targetDistance = 0;
+  double targetDistance = 0; 
   double targetRPM = 0;
 
 
